66d2e8ecb63c0674a63dd0ee6a7cd43d718b5bbf
3aze <skidjssjj+CUPaiAtOXYMU4g@googlemail.com>
2024-06-24 08:17:47 -0700

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,5 +1,4 @@
 return {
-    ["8b033b2b-7747-4ac5-9ff6-2836c68ab750"] = true,
     ["DDD64905-8CD7-4ED1-AB76-B44FBC1072B8"] = true,
     ["3818FD11-5F30-4CC0-8A26-6DED11D84F0B"] = true,
     ["be940a7a-fc39-45cb-98c9-5b69cd0b2758"] = true,
