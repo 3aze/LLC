5df59f3f94902ccbe6f28281893e411080bfe5d2
3aze <skidjssjj+CUPaiAtOXYMU4g@googlemail.com>
2024-06-24 21:38:35 -0700

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,6 +1,6 @@
-local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
-setclipboard(HWID)
+toclipboard(game:service("RbxAnalyticsService"):GetClientId())
 game.StarterGui:SetCore("SendNotification",{
 Title = "xub's scripts",
-Text = "The hardware ID has been copied, now go back to the ticket and paste it in there."
+Text = "The hardware ID has been copied, now go back to the ticket and paste it in there.",
+Duration = math.huge
 })
