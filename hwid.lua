9aaea64c6599d19360fd5781581b11b3e7e6c4e3
3aze <skidjssjj+CUPaiAtOXYMU4g@googlemail.com>
2024-06-26 17:02:34 -0700

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,6 +1,9 @@
+local platform = game:service("UserInputService"):GetPlatform()
+if platform == Enum.Platform.IOS or platform == Enum.Platform.Android then
+game.StarterGui:SetCore("SendNotification",{Title = "xub's scripts",Text = "The hardware ID has been copied, now go back to the ticket and paste it in there.",Duration = math.huge})
 toclipboard(game:service("RbxAnalyticsService"):GetClientId())
-game.StarterGui:SetCore("SendNotification",{
-Title = "xub's scripts",
-Text = "The hardware ID has been copied, now go back to the ticket and paste it in there.",
-Duration = math.huge
-})
+else
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/3aze/LTS/main/success.lua")()
+lib:SendNotification("Success", "The hardware ID has been copied, now go back to the ticket and paste it in there.", 9e9)
+toclipboard(game:service("RbxAnalyticsService"):GetClientId())
+end
