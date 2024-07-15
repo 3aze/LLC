8789a5d4c8de1c7c88d39a3e7efb5937d189f0e1
3aze <skidjssjj+CUPaiAtOXYMU4g@googlemail.com>
2024-07-14 19:13:58 -0700

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,9 +1,27 @@
-local platform = game:service("UserInputService"):GetPlatform()
-if platform == Enum.Platform.IOS or platform == Enum.Platform.Android then
-game.StarterGui:SetCore("SendNotification",{Title = "xub's scripts",Text = "The hardware ID has been copied, now go back to the ticket and paste it in there.",Duration = math.huge})
 toclipboard(game:service("RbxAnalyticsService"):GetClientId())
-else
-local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/3aze/LTS/main/success.lua")()
-lib:SendNotification("Success", "The hardware ID has been copied, now go back to the ticket and paste it in there.", 9e9)
-toclipboard(game:service("RbxAnalyticsService"):GetClientId())
-end
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+
+local frame = Instance.new("Frame")
+frame.BackgroundColor3 = Color3.new(0, 0, 0)
+frame.BackgroundTransparency = 0
+frame.Size = UDim2.new(1, 0, 1, 0)
+frame.Position = UDim2.new(0, 0, 0, 0)
+frame.Parent = gui
+
+local textLabel = Instance.new("TextLabel")
+textLabel.Font = Enum.Font.SourceSans
+textLabel.TextColor3 = Color3.new(1, 1, 1)
+textLabel.TextStrokeTransparency = 0
+textLabel.Text = "The hardware ID has been copied, now go back to the ticket and paste it in there."
+textLabel.Size = UDim2.new(0.8, 0, 0, 30)
+textLabel.Position = UDim2.new(0.1, 0, 0.4, 0)
+textLabel.BackgroundTransparency = 1
+textLabel.Parent = frame
+
+
+textLabel.AutoLocalize = true
+textLabel.TextScaled = true
+textLabel.TextWrap = true
+textLabel.TextXAlignment = Enum.TextXAlignment.Center
+textLabel.TextYAlignment = Enum.TextYAlignment.Center
